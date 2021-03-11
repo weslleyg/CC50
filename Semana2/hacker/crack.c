9af5a75ff1ccd5c00145ce66fb483a6f7fca92ca
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-03-11 18:05:35 -0300

    hash

--- Semana2/hacker/crack.c
+++ Semana2/hacker/crack.c
@@ -3,28 +3,29 @@
 #include <stdio.h>
 #include <crypt.h>
 
-int max_key = 4;
+int max_key = 9;
 bool crackeado;
-char resultado[6];
+char resultado[10];
 
-void cracking(string input, string salt, char *tentativa, int index, int letra);
+void cracking(string input, string salt, char *tentativa, int index, int nLetras);
 
 int main(int argc, char *argv[]) {
   //string pass = argv[1];
 
   if(argc == 1 || argc == 3) {
+    printf("Argumento invalido!");
     return 1;
   }
   //SALT: first 2 char in a hashed password
   //Crypt: (key, salt)
   char salt[3] = {argv[1][0], argv[1][1], '\0'};
-  printf("%s", salt);
+  // printf("%s", salt);
 
-  for(int letra = 1, n = max_key + 2; letra < n; letra++) {
-    char tentativa[letra + 1];
-    tentativa[letra] = '\0';
+  for(int nLetras = 1, n = max_key + 2; nLetras < n; nLetras++) {
+    char tentativa[nLetras + 1];
+    tentativa[nLetras] = '\0';
 
-    cracking(argv[1], salt, tentativa, 0, letra);
+    cracking(argv[1], salt, tentativa, 0, nLetras);
 
     if(crackeado) {
       printf("%s", resultado);
@@ -37,12 +38,12 @@ int main(int argc, char *argv[]) {
 
 }
 
-void cracking(string input, string salt, char *tentativa, int index, int letra) {
-  if(index < letra - 1) {
+void cracking(string input, string salt, char *tentativa, int index, int nLetras) {
+  if(index < nLetras - 1) {
     for(char c = 'a'; c <= 'z'; c++) {
       tentativa[index] = c;
 
-      cracking(input, salt, tentativa, index +1, letra);
+      cracking(input, salt, tentativa, index +1, nLetras);
 
       if(crackeado) return;
     }
@@ -50,15 +51,17 @@ void cracking(string input, string salt, char *tentativa, int index, int letra) 
     for(char c = 'A'; c <= 'Z'; c++) {
       tentativa[index] = c;
 
-      cracking(input, salt, tentativa, index +1, letra);
+      cracking(input, salt, tentativa, index +1, nLetras);
 
       if(crackeado) return;
     }
   } else {
+    // printf("%s", tentativa);
     string hash = crypt(tentativa, salt);
     bool resultTentativa = true;
 
     for(int i = 0; i < 13; i++) {
+      //Compara as hashes
       if(hash[i] != input[i]) {
         resultTentativa = false;
         break;
@@ -68,7 +71,7 @@ void cracking(string input, string salt, char *tentativa, int index, int letra) 
     if(resultTentativa) {
       crackeado = true;
       
-      for(int i = 0; i < letra; i++) {
+      for(int i = 0; i < nLetras; i++) {
         resultado[i] = tentativa[i];
       }
     }
---
+++ Semana2/hacker/hash.c
@@ -0,0 +1,11 @@
+#include <cc50.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <crypt.h>
+
+int main(int argc, char *argv[]) {
+  string pass = argv[1];
+  string hash = crypt(pass, "50");
+
+  printf("%s %s", hash, pass);
+}

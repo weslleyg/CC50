047e1cdc0805eb26a129ea7e027ef752614dfc4a
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-04-12 21:35:37 -0300

    sell ok

--- Semana7/pset7/sell2.php
+++ Semana7/pset7/sell2.php
@@ -8,18 +8,18 @@
     if($s === NULL)
         apologize("Ticket de ação inválido!");
 
-  $result = mysqli_query($connection, "SELECT shares FROM stocks WHERE userId = {$_SESSION["uid"]} ORDER BY symbol = $symbol");
+  $result = mysqli_query($connection, "SELECT shares FROM stocks WHERE userId = {$_SESSION["uid"]} AND symbol = '{$symbol}'");
   
   $shares = mysqli_fetch_array($result);
 
   $value = $shares["shares"] * $s->price;
 
-  mysqli_query($connection, "DELETE FROM stocks WHERE userId = {$_SESSION["uid"]} AND symbol = {$symbol}");
+  mysqli_query($connection, "DELETE FROM stocks WHERE userId = {$_SESSION["uid"]} AND symbol = '{$symbol}'");
 
   mysqli_query($connection, "UPDATE users SET cash = cash + $value WHERE uid = {$_SESSION["uid"]}");
 
 ?>
 
 <div style="text-align: center">
-  Venda efetuada com sucesso no valor de R$<?= $shares["shares"] ?>!
+  Venda efetuada com sucesso no valor de R$<?= $value ?>!
 </div>
